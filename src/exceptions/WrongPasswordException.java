521b1ea03726614adbe18b4967889c908269651f
Evancel <w.lexandra@gmail.com>
2023-03-25 13:14:57 +0300

    The first commit

---
+++ src/exceptions/WrongPasswordException.java
@@ -0,0 +1,9 @@
+package exceptions;
+import java.lang.RuntimeException;
+
+public class WrongPasswordException extends RuntimeException {
+    public WrongPasswordException(){
+        super();
+    }
+}
+
